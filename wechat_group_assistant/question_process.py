355275aedfaf37de10e1454b8db186c243d5610c
Suiseiseki-2016 <wd050602@gmail.com>
2025-03-25 19:18:43 +0800

    Add the final filter

--- wechat_group_assistant/question_process.py
+++ wechat_group_assistant/question_process.py
@@ -3,6 +3,7 @@ from wechat_group_assistant.utils.prompt import prompt
 
 def process_question(question: str) -> str:
     print ("Handle question: ", question)
-    result = complex_chat(prompt["quesiton_answer"], question)
+    result = complex_chat(prompt["quesiton_answer"] + "\n" + prompt["question_info"], question)
+    result = simple_chat(prompt["question_parser"], result)
     print ("Process Result: ", result)
     return result
